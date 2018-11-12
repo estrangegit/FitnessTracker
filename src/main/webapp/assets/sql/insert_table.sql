167c830f2ca5958530ab433ec76576056df91f24
estrangegit <etienne.estrangin@gmail.com>
2018-11-12 21:49:55 +0100

    5.11 Spring Security Client Integration

---
+++ src/main/webapp/assets/sql/insert_table.sql
@@ -0,0 +1,7 @@
+insert into users (username, password, enabled) values ("bryan", "secret", true);
+
+insert into authorities (username, authority) values ("bryan", "ROLE_USER");
+
+insert into users (username, password, enabled) values ("chris", "secret", true);
+
+insert into authorities (username, authority) values ("chris", "ROLE_ADMIN");
