66755e5e8b4a182f8215eb19a39848ffce1a3879
Etienne Estrangin <etienne.estrangin@cognizant.com>
2018-11-13 14:35:19 +0100

    Adding ROLE_USER to all users in database

--- src/main/webapp/assets/sql/insert_table.sql
+++ src/main/webapp/assets/sql/insert_table.sql
@@ -5,3 +5,5 @@ insert into authorities (username, authority) values ("bryan", "ROLE_USER");
 insert into users (username, password, enabled) values ("chris", "secret", true);
 
 insert into authorities (username, authority) values ("chris", "ROLE_ADMIN");
+
+insert into authorities (username, authority) values ("chris", "ROLE_USER");
